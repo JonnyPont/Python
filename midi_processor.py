bdbd014a25c200185cff4cb2cfe13eff2f310149
Jon Pont <jp15101@bristol.ac.uk>
2018-10-16 15:05:11 +0100

    note table added with note addition and removal functions

--- midi_processor.py
+++ midi_processor.py
@@ -15,7 +15,6 @@ import pygame
 
 import pygame.midi
 
-
 #Needs initialising for some reason, and declaring that events need to be got
 pygame.init()
 
@@ -35,6 +34,10 @@ output_id = pygame.midi.get_default_output_id()
 in_midi_device = pygame.midi.Input( input_id )
 out_midi_device = pygame.midi.Output(output_id)
 
+#Creates an empty note table
+empty_midi_byte = [0,0,0,0]
+note_table = [empty_midi_byte] * 10
+
 
 #Run forever
 while True:
@@ -42,13 +45,36 @@ while True:
     if in_midi_device.poll():
 #        store the first 10 new midi messages
         midi_events = in_midi_device.read(10)
-        #print received midi messages
-        for j in range(len(midi_events)):
-            print(midi_events[0][j])
-            print(midi_events[0][0][0],midi_events[0][0][1],midi_events[0][0][2])
-            out_midi_device.write_short(midi_events[0][0][0],midi_events[0][0][1],midi_events[0][0][2])
+        
         if midi_events[0][0] == [144, 72, 1, 0]:
             break
+        
+        #print received midi messages and send to output (windows' own synth)
+        for i in range(len(midi_events)):
+            if midi_events[i][0][0] == 144: #if note on
+                '''Add note to notetable here'''
+                for j in range(8,-1,-1):
+                    if note_table[j][2] != 0:
+                        note_table[j+1] = note_table[j]                         
+                note_table[j] = midi_events[i][0]
+                note_table[0] = midi_events[i][0]
+            elif midi_events[i][0][0] == 128: #if note off
+                '''Remove note from notetable here''' 
+                for j in range(len(note_table)):
+                    if midi_events[i][0][1] == note_table[j][1]:
+                        for k in range(j,9):
+                            if k < 9:
+                                note_table[k] = note_table[k+1]  
+                                print(k+1)
+                note_table[9] = empty_midi_byte              
+#            print(midi_events[0][i])
+            for byte in note_table:
+                print(*byte)    
+            out_midi_device.write_short(midi_events[0][0][0],midi_events[0][0][1],midi_events[0][0][2])
+        print('\n')    
+        
+        
+        
         
 
 #Close off all of the opened channels and exit the initialisations.
@@ -57,4 +83,3 @@ in_midi_device.close()
 pygame.midi.quit()
 
 pygame.quit()
-
