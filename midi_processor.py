0fa5b440a04857cd1a2198e35e89d2881f80852a
Jon Pont <jp15101@bristol.ac.uk>
2018-10-16 13:46:30 +0100

    midi output added

--- midi_processor.py
+++ midi_processor.py
@@ -29,26 +29,30 @@ event_post = pygame.fastevent.post
 pygame.midi.init()
 
 input_id = pygame.midi.get_default_input_id()
+output_id = pygame.midi.get_default_output_id()
 
-i = pygame.midi.Input( input_id )
 
+in_midi_device = pygame.midi.Input( input_id )
+out_midi_device = pygame.midi.Output(output_id)
 
 
 #Run forever
 while True:
 #    if a message exists
-    if i.poll():
+    if in_midi_device.poll():
 #        store the first 10 new midi messages
-        midi_events = i.read(10)
+        midi_events = in_midi_device.read(10)
         #print received midi messages
         for j in range(len(midi_events)):
             print(midi_events[0][j])
-
-
-
+            print(midi_events[0][0][0],midi_events[0][0][1],midi_events[0][0][2])
+            out_midi_device.write_short(midi_events[0][0][0],midi_events[0][0][1],midi_events[0][0][2])
+        if midi_events[0][0] == [144, 72, 1, 0]:
+            break
+        
 
 #Close off all of the opened channels and exit the initialisations.
-i.close()
+in_midi_device.close()
 
 pygame.midi.quit()
 
